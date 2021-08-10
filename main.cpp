a1abae135f29c2b0b4c666db73192ec323f970d3
laiyuling <laiyuling424@163.com>
2021-08-10 14:26:29 +0800

    5th

--- main.cpp
+++ main.cpp
@@ -39,26 +39,26 @@ namespace second_space {
 }
 
 
-int main() {
-
-//    first_space::func();
-//    second_space::func();
+//int main() {
 //
-//    int i = 1;
-//    int&b = i;
-//    int c = i;
-//    printf("\ni is %d,&b is %d,c is %d",&i,&b,&c);
-//    //i is -274684040,&b is -274684040,c is -274684052
-
-    set<int> list;
-    pair<set<int>::iterator, bool> a = list.insert(1);
-    cout << a.second << endl;
-    a = list.insert(1);
-    cout << a.second << endl;
-    a = list.insert(2);
-    cout << a.second << endl;
-    a = list.insert(2);
-    cout << a.second << endl;
-
-    return 0;
-}
+////    first_space::func();
+////    second_space::func();
+////
+////    int i = 1;
+////    int&b = i;
+////    int c = i;
+////    printf("\ni is %d,&b is %d,c is %d",&i,&b,&c);
+////    //i is -274684040,&b is -274684040,c is -274684052
+//
+//    set<int> list;
+//    pair<set<int>::iterator, bool> a = list.insert(1);
+//    cout << a.second << endl;
+//    a = list.insert(1);
+//    cout << a.second << endl;
+//    a = list.insert(2);
+//    cout << a.second << endl;
+//    a = list.insert(2);
+//    cout << a.second << endl;
+//
+//    return 0;
+//}
